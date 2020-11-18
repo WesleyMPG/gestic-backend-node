5372be1de549341e4a0fc3efe9d35aed0dfcb76e
Alvaro <alvaro.aamorim98@gmail.com>
2020-11-18 16:28:53 -0300

    added name attribute

--- docs/operations/PROFILES.sql
+++ docs/operations/PROFILES.sql
@@ -12,6 +12,7 @@ INSERT INTO profiles VALUES ('21f3392d-fbdc-4cb0-8e3c-5a11d6c069f1', 'COOR', 'CO
 INSERT INTO users VALUES (
   'f9584d5c-b11e-4148-8e12-e124782f9b9c', 
   '3a2744c1-fa73-43f1-bceb-a8cee76e5f35',
+  'aluno1',
   '12345678910',
   'aluno1@ic.ufal.br',
   'aluno111');
@@ -19,6 +20,7 @@ INSERT INTO users VALUES (
 INSERT INTO users VALUES (
   '2e39fcf9-46cc-48d4-b24a-af755ea80117', 
   '3a2744c1-fa73-43f1-bceb-a8cee76e5f35',
+  'aluno2',
   '321.654.987-11',
   'aluno2@ic.ufal.br',
   'aluno222');
@@ -26,6 +28,7 @@ INSERT INTO users VALUES (
 INSERT INTO users VALUES (
   'f5884dee-ec53-4494-915a-e1676af78d15', 
   '0868fcf1-2ade-43f0-806c-2952409c579a',
+  'professor1',
   '111.222.333-23',
   'prof1@ic.ufal.br',
   'prof111');
@@ -33,6 +36,7 @@ INSERT INTO users VALUES (
 INSERT INTO users VALUES (
   '6bbe1d58-8b7d-4209-8fa7-a12522794708', 
   '21f3392d-fbdc-4cb0-8e3c-5a11d6c069f1',
+  'professor2',
   '11111111111',
   'prof2@ic.ufal.br',
   'prof222');
