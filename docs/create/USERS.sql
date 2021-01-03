963c45a4158eb4bf4197fff1e7a338556fa39f66
Alvaro <alvaro.aamorim98@gmail.com>
2021-01-03 20:17:48 -0300

    fixed error in USERS.sql create file

--- docs/create/USERS.sql
+++ docs/create/USERS.sql
@@ -4,11 +4,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users (
   user_id UUID DEFAULT uuid_generate_v4 (),
-  prod_id UUID NOT NULL,
+  prof_id UUID NOT NULL,
   user_name VARCHAR(200) NOT NULL,
   user_cpf VARCHAR(15) NOT NULL UNIQUE,
   user_email VARCHAR(200) NOT NULL UNIQUE,
   user_password VARCHAR(200) NOT NULL,
   CONSTRAINT pk_user PRIMARY KEY (user_id),
-  CONSTRAINT fk_user_prof FOREIGN KEY (prod_id) REFERENCES profiles (prof_id) ON DELETE CASCADE
+  CONSTRAINT fk_user_prof FOREIGN KEY (prof_id) REFERENCES profiles (prof_id) ON DELETE CASCADE
 );
